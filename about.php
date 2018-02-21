445a07cdee8a12183a1e8d1d7891a9bc9817a5c3
Harmel Ngongo <harmelngongo@gmail.com>
2018-02-21 16:33:41 +0300

    mpesa
    mpesa

--- about.php
+++ about.php
@@ -14,10 +14,10 @@
       <!-- Intro Content -->
       <div class="row">
         <div class="col-lg-6">
-          <img class="img-fluid rounded mb-4" src="img/boeing9.jpeg" alt="Captain Jay">
+          <img class="img-fluid rounded mb-4" src="img/cap.jpg" alt="Captain Jay">
         </div>
         <div class="col-lg-6">
-          
+          <br>
           <p> Basically I am <b>JOUVAIN KAPUTU TSHIENDA</b> as my full familly name but know as Jay or Joe by so many. I am a young man passionated by the aviation field , particularly in piloting , so far I am <b>ppl holder</b> , and I am on my way to get my commercial licence , so I am pursuing my childhood dream which was so clear to me at very early age , to become an airline pilot.</p>
 
           <p> I am on the move fulfilling my goals and I believe I will achieve my highest desire specially given to me compare to most fields among friends and family , and here I wanna take pleasure in sharing the flame that burns and gives me thirst regarding aviation industry...</p>
@@ -34,7 +34,7 @@
           <div class="card h-100 text-center">
             <img class="card-img-top" src="img/boeing9.jpeg" alt="">
             <div class="card-body">
-              <h4 class="card-title">Team Member</h4>
+              <h4 class="card-title">Kezai Rebz</h4>
               <h6 class="card-subtitle mb-2 text-muted">Position</h6>
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
             </div>
@@ -47,7 +47,7 @@
           <div class="card h-100 text-center">
             <img class="card-img-top" src="img/boeing9.jpeg" alt="">
             <div class="card-body">
-              <h4 class="card-title">Team Member</h4>
+              <h4 class="card-title">Frazia Sparkle</h4>
               <h6 class="card-subtitle mb-2 text-muted">Position</h6>
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
             </div>
@@ -60,7 +60,7 @@
           <div class="card h-100 text-center">
             <img class="card-img-top" src="img/boeing9.jpeg" alt="">
             <div class="card-body">
-              <h4 class="card-title">Team Member</h4>
+              <h4 class="card-title">Harmel Ngongo</h4>
               <h6 class="card-subtitle mb-2 text-muted">Position</h6>
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
             </div>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         </ol>
         <div class="carousel-inner" role="listbox">
           <!-- Slide One - Set the background image for this slide in the line below -->
-          <div class="carousel-item active" style="background-image: url('img/boeing7.jpeg')">
+          <div class="carousel-item active" style="background-image: url('img/plain.jpg')">
             <div class="carousel-caption d-none d-md-block">
               <h1>Example headline.</h1>
               <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
@@ -20,7 +20,7 @@
             </div>
           </div>
           <!-- Slide Two - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('img/boein0.png')">
+          <div class="carousel-item" style="background-image: url('img/boeing7.jpeg')">
             <div class="carousel-caption d-none d-md-block">
               <h1>Example headline.</h1>
               <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
@@ -50,27 +50,27 @@
     <!-- Page Content -->
     <div class="container">
 
-      <h1 class="my-4"><font face = "Georgia" color="#007bff"><center>Welcome to <u>Captain Jouvain</u></center></font></h1>
-
+      <h1 class="my-4"><font face = "Georgia" color="#007bff"><center>Welcome to our <u>services</u></center></font></h1>
+      <br>
       <!-- Marketing Icons Section -->
       <div class="row">
         <div class="col-lg-4">
-          <img class="img-circle" src="img/news-1.jpg" alt="Generic placeholder image" width="200" height="200" data-square="19600">
-          <h2><font face = "Georgia" color = "#007bff">Roadway Freight</font></h2>
+          <img class="img-circle" src="img/adv.png" alt="Generic placeholder image" width="200" height="200" data-square="19600">
+          <h2><font face = "Georgia" color = "#007bff">Company advertising</font></h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="https://getbootstrap.com/docs/3.3/examples/carousel/#" role="button">View details »</a></p>
+          <p><a class="btn btn-default" href="" role="button">View details »</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="img/news-2.jpg" alt="Generic placeholder image" width="200" height="200" data-square="19600">
-          <h2><font face = "Georgia" color = "#007bff">Air Freight</font></h2>
+          <img class="img-circle" src="img/consu.png" alt="Generic placeholder image" width="200" height="200" data-square="19600">
+          <h2><font face = "Georgia" color = "#007bff">Business Consultancy</font></h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="https://getbootstrap.com/docs/3.3/examples/carousel/#" role="button">View details »</a></p>
+          <p><a class="btn btn-default" href="" role="button">View details »</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="img/news-4.jpg" alt="Generic placeholder image" width="200" height="200" data-square="19600">
-          <h2><font face = "Georgia" color = "#007bff">Ocean Freight</font></h2>
+          <img class="img-circle" src="img/shop.png" alt="Generic placeholder image" width="200" height="200" data-square="19600">
+          <h2><font face = "Georgia" color = "#007bff">Selling</font></h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="https://getbootstrap.com/docs/3.3/examples/carousel/#" role="button">View details »</a></p>
+          <p><a class="btn btn-default" href="" role="button">View details »</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
       <!-- /.row -->
